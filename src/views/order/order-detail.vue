01faa5610e92e89d464b524b28c864686154e5b0
laozhibin <893435967@qq.com>
2019-09-25 13:01:55 +0800

    1.转订单页面,试乘试驾页面时间选择,列表选择代码优化 2.新增moment

--- src/views/order/order-detail.vue
+++ src/views/order/order-detail.vue
@@ -3,13 +3,13 @@
 		.container
 			.cell
 				van-cell-group
-					van-field(readonly label="客户名称" )
-					van-field(readonly label="客户电话" required)
-					van-field(readonly label="意向类型" required)
-					van-field(readonly label="证件类型" required)
+					van-field(v-model="page.name" readonly label="客户名称" )
+					van-field(v-model="page.phone" readonly label="客户电话" required)
+					van-field(v-model="page.willing" readonly label="意向类型" required)
+					van-field(readonly placeholder="请选择"  label="证件类型" required :right-icon="rightIcon")
 					van-field(placeholder="请输入" label="证件号码" required)
 					van-field(placeholder="请输入" label="证件签发机关" required)
-					van-field(v-model="availableTimeStr" placeholder="请选择" label="证件有效期" :right-icon="rightIcon" required @click-right-icon="() => {showPopup('availableTime')}")
+					van-field(v-model="page.availableTime" placeholder="请选择" label="证件有效期" :right-icon="rightIcon" required @click-right-icon="() => {showPopup('availableTime')}")
 			.cell
 				van-cell-group
 					van-field(placeholder="请输入" label="联系人" required)
@@ -18,8 +18,8 @@
 					van-field(placeholder="请输入" label="详细地址" required)
 			.cell
 				van-cell-group
-					van-field(v-model="signTimeStr" placeholder="请选择" label="签约日期" :right-icon="rightIcon" required @click-right-icon="() => {showPopup('signTime')}")
-					van-field(v-model="deliveryTimeStr" placeholder="请选择" label="承诺交车日期" :right-icon="rightIcon" required @click-right-icon="() => {showPopup('deliveryTime')}")
+					van-field(v-model="page.signTime" placeholder="请选择" label="签约日期" :right-icon="rightIcon" required @click-right-icon="() => {showPopup('signTime')}")
+					van-field(v-model="page.deliveryTime" placeholder="请选择" label="承诺交车日期" :right-icon="rightIcon" required @click-right-icon="() => {showPopup('deliveryTime')}")
 					van-field(readonly placeholder="请选择" label="是否分网销" required :right-icon="rightIcon")
 			.cell
 				van-cell-group
@@ -32,32 +32,66 @@
 
 
 		van-popup(
-			v-model="show"
+			v-model="date.show"
 			position="bottom"
 		)
 			van-datetime-picker(
-				v-model="popupTimeSelect"
+				v-model="date.popupTimeSelect"
 				type="date"
 				@confirm="dateConfirm"
 				@cancel="dateCancel"
 			)
+		van-action-sheet(
+			v-model="select.selectShow"
+			:actions="actionItems"
+			@select="selectItem"
+		)
 </template>
 
 <script>
+	import moment from 'moment'
     export default {
         name: 'order-detail',
 		props: ['orderId', 't', 'someId'], // orderId: 订单ID 转订单时为-1 t: 编辑或者查看（经理只能查看） someId: 备用参数
+		created() {
+			this.setDefaultTime();
+		},
 		data() {
         	return {
-				currentOperateTimeType: '', // 判断是证件有效期 签约日期 承诺交车日期
-				popupTimeSelect: new Date(),
-				show: false, // 控制时间选择的显示隐藏
-				selectShow: false, // 控制上弹选项的显示隐藏
-				currentSelectType: '', // 当前选择的是哪种上弹类型
-				availableTime: null,
-				signTime: new Date(), // 签约日期 默认当天
-				deliveryTime: null // 交车日期
-
+				page: {
+					name: '吴彦祖',
+					phone: '18888888888',
+					willing: '博越',
+					credentialType: '',
+					availableTime: '',
+					signTime: '',
+					deliveryTime: ''
+				},
+				date: {
+					timeFormatRule: {
+						datetime: 'YYYY-MM-DD HH:mm',
+						date: 'YYYY-MM-DD'
+					},
+					currentOperateTimeType: '', // 判断是证件有效期 签约日期 承诺交车日期
+					show: false, // 控制时间选择的显示隐藏
+					popupTimeSelect: new Date(),
+				},
+				select: {
+					selectShow: false, // 控制上弹选项的显示隐藏
+					currentSelectType: '', // 当前选择的是哪种上弹类型
+					items: {
+						credentialType: [
+							{ name: '博越' },
+							{ name: '星越' },
+							{ name: '缤越' }
+						],
+						people: [
+							{ name: '胡彦祖' },
+							{ name: '吴彦兵' },
+							{ name: '胡京' }
+						]
+					}
+				}
 			}
 		},
 		computed: {
@@ -75,74 +109,57 @@
 					}
         		}
         	},
-			availableTimeStr() { // 有效日期字符串
-				return this.timeFormat({
-					date: this.availableTime,
-					value: ''
-				})
+			actionItems() {
+				return this.select.items[this.select.currentSelectType];
 			},
-			signTimeStr() { // 签约日期字符串
-				return this.timeFormat({
-					date: this.signTime,
-					value: ''
-				})
-			},
-			deliveryTimeStr() { // 交车日期字符串
-				return this.timeFormat({
-					date: this.deliveryTime,
-					value: ''
-				})
+			rule() {
+				return this.date.timeFormatRule.date;
 			}
 		},
 		methods: {
+			/* 时间选择相关-start */
 			showPopup(type) { // 显示时间选择框
-				this.currentOperateTimeType = type;
-				this.popupTimeSelect = this[type];
-				this.show = true;
+				this.date.currentOperateTimeType = type;
+				this.date.popupTimeSelect = this.timeFormat(this.page[type], this.rule);
+				this.date.show = true;
 			},
 			dateConfirm(value) { // 确认时间选择
-				switch (this.currentOperateTimeType) {
-					case 'availableTime':
-						this.availableTime = value;
-						break;
-					case 'signTime':
-						this.signTime = value;
-						break;
-					case 'deliveryTime':
-						this.deliveryTime = value;
-						break;
-					default:
-						break;
-				}
-				this.show = false;
+				this.page[this.date.currentOperateTimeType] = this.timeFormat(value, this.rule);
+				this.dateCancel();
 			},
 			dateCancel() { // 取消时间选择
-				this.show = false;
+				this.date.show = false;
 			},
-			timeFormat({date, value}) { // Date转str
-				if ((this.orderId !== '-1' && this.t === 'show')) { // 判断是编辑页还是详情页
-					return value;
+			timeFormat(value, rule) {
+				if (!value) { // 为空时日期选择弹出来没有默认时间，默认 是默认的最小时间2009年1月1日
+					return null;
 				} else {
-					if (date !== null) {
-						let str = '';
-						let dateObj = {
-							year: date.getFullYear(),
-							month: date.getMonth() + 1,
-							day: date.getDate(),
-						};
-						const { year, month, day } = dateObj;
-						const monthFormat = this.numberFormat(month);
-						const dayFormat = this.numberFormat(day);
-						str = `${year}-${monthFormat}-${dayFormat}`;
-						return str
-					} else {
-						return '';
+					if (value instanceof Date) { // 如果传进来的是日期对象 根据rule（类似YYYY-MM-DD HH:mm:ss）将其转成
+						return moment(value).format(rule);
+					} else if (typeof value === 'string') { // 如果是字符串 则转成日期对象
+						return new Date(moment(value, rule).valueOf());
 					}
 				}
+
 			},
-			numberFormat(number) {
-				return number < 10 ? `0${number}` : number;
-			}
+			getCurrentDateStr(rule) {
+				return moment(new Date()).format(rule);
+			},
+			setDefaultTime() {
+				this.page.signTime = this.getCurrentDateStr(this.rule); // 试驾时间默认为当天
+			},
+			/* 时间选择相关-end */
+
+			/* 类型选择-start */
+			openSelect(type) {
+				this.select.currentSelectType = type
+				this.select.selectShow = true;
+			},
+			selectItem(item) { // 选择车型
+				this.select.selectShow = false;
+				this.page[this.select.currentSelectType] = item.name;
+			},
+			/* 类型选择-end */
 		}
     }
 </script>
--- src/views/try-car/detail.vue
+++ src/views/try-car/detail.vue
@@ -1,41 +1,41 @@
 <template lang="pug">
 	m-page(:head-title="title").try-car-detail
-		.container(:class="{ noButton: this.routeType === 'show' }")
+		.container(:class="{ noButton: routeType === 'show' }")
 			.cell
 				van-cell-group
-					van-field(v-model="name" readonly label="客户名称" required)
-					van-field(v-model="phone" readonly label="客户电话" required :right-icon="rightIcon" @click-right-icon="toBeContinued")
-					van-field(v-model="car" readonly label="车牌车型号" placeholder="请选择" required :right-icon="rightIcon" @click-right-icon="() => { openSelect('car') }")
-					van-field(v-model="tryTimeStr" readonly label="试驾时间" required :right-icon="rightIcon" @click-right-icon="() => {showPopup('tryTime')} ")
+					van-field(v-model="page.name" readonly label="客户名称" required)
+					van-field(v-model="page.phone" readonly label="客户电话" required :right-icon="rightIcon" @click-right-icon="toBeContinued")
+					van-field(v-model="page.car" readonly label="车牌车型号" placeholder="请选择" required :right-icon="rightIcon" @click-right-icon="() => { openSelect('car') }")
+					van-field(v-model="page.tryTime" readonly label="试驾时间" required :right-icon="rightIcon" @click-right-icon="() => {showPopup('tryTime')} ")
 			.cell
 				van-cell-group
 					van-field(v-model="hasUploaded" readonly label="身份证照片" placeholder="请上传" required right-icon="arrow"  @click-right-icon="() => { goToUpload('id') }")
-					van-field(v-model="id" label="身份证号" placeholder="请输入" required)
-					van-field(v-model="sex" readonly label="性别")
-					van-field(v-model="birth" readonly label="出生日期")
-					van-field(v-model="address" label="住址" placeholder="请输入" required)
+					van-field(v-model="page.id" label="身份证号" placeholder="请输入" required)
+					van-field(v-model="page.sex" readonly label="性别")
+					van-field(v-model="page.birth" readonly label="出生日期")
+					van-field(v-model="page.address" label="住址" placeholder="请输入" required)
 			.cell
 				van-cell-group
 					van-field(v-model="hasUploaded" readonly label="驾照照片" placeholder="请上传" required right-icon="arrow"  @click-right-icon="() => { goToUpload('dl') }")
-					van-field(v-model="startTimeStr" readonly label="生效日期" placeholder="请选择" required :right-icon="rightIcon"  @click-right-icon="() => {showPopup('startTime')} ")
-					van-field(v-model="endTimeStr" readonly label="截止日期" placeholder="请选择" required :right-icon="rightIcon"  @click-right-icon="() => {showPopup('endTime')} ")
+					van-field(v-model="page.startTime" readonly label="生效日期" placeholder="请选择" required :right-icon="rightIcon"  @click-right-icon="() => {showPopup('startTime')} ")
+					van-field(v-model="page.endTime" readonly label="截止日期" placeholder="请选择" required :right-icon="rightIcon"  @click-right-icon="() => {showPopup('endTime')} ")
 			.cell
 				van-cell-group
-					van-field(v-model="protocol" readonly label="试驾协议" required right-icon="arrow"  @click-right-icon="toBeContinued")
-					van-field(v-model="people" readonly label="试驾人员" placeholder="请选择" required :right-icon="rightIcon"  @click-right-icon="() => { openSelect('people') }")
-		.bottom-button(v-if="this.routeType !== 'show'" @click="doTryCar") 立即试驾
+					van-field(v-model="page.protocol" readonly label="试驾协议" required right-icon="arrow"  @click-right-icon="toBeContinued")
+					van-field(v-model="page.people" readonly label="试驾人员" placeholder="请选择" required :right-icon="rightIcon"  @click-right-icon="() => { openSelect('people') }")
+		.bottom-button(v-if="routeType !== 'show'" @click="doTryCar") 立即试驾
 		van-popup(
-			v-model="show"
+			v-model="date.show"
 			position="bottom"
 		)
 			van-datetime-picker(
-				v-model="popupTimeSelect"
+				v-model="date.popupTimeSelect"
 				:type="timeSelectType"
 				@confirm="dateConfirm"
 				@cancel="dateCancel"
 			)
 		van-action-sheet(
-			v-model="selectShow"
+			v-model="select.selectShow"
 			:actions="actionItems"
 			@select="selectItem"
 		)
@@ -44,28 +44,53 @@
 
 <script>
 	import { Dialog } from 'vant';
+	import moment from 'moment'
 	export default {
 		name: 'detail',
+		created() {
+			this.setDefaultTime();
+		},
 		data() {
 			return {
-				name: '胡歌',
-				phone: '123456',
-				car: '',
-				id: '',
-				sex: '男',
-				birth: '1988-12',
-				address: '',
-				protocol: '已上传', // 试驾协议
-				people: '', // 试驾人员
-
-				currentOperateTimeType: '',
-				popupTimeSelect: new Date(),
-				tryTimeHistory: new Date(), // 选择时间确认之后的值
-				startTimeHistory: new Date(),
-				endTimeHistory: new Date(),
-				show: false,
-				selectShow: false,
-				currentSelectType: ''
+				page: {
+					name: '胡歌',
+					phone: '123456',
+					car: '',
+					id: '',
+					sex: '男',
+					birth: '1988-12',
+					address: '',
+					protocol: '已上传', // 试驾协议
+					people: '', // 试驾人员
+					tryTime: '',
+					startTime: '',
+					endTime: ''
+				},
+				date: {
+					timeFormatRule: {
+						datetime: 'YYYY-MM-DD HH:mm',
+						date: 'YYYY-MM-DD'
+					},
+					currentOperateTimeType: '',
+					show: false,
+					popupTimeSelect: new Date()
+				},
+				select: {
+					selectShow: false,
+					currentSelectType: '',
+					items: {
+						car: [
+							{ name: '博越' },
+							{ name: '星越' },
+							{ name: '缤越' }
+						],
+						people: [
+							{ name: '胡彦祖' },
+							{ name: '吴彦兵' },
+							{ name: '胡京' }
+						]
+					}
+				}
 			}
 		},
 		props: {
@@ -78,147 +103,80 @@
 		},
 		computed: {
 			title() {
-				console.log(this.detailId)
+				// console.log(this.detailId)
 				return this.routeType === 'show' ? '试乘试驾详情' : '确认试乘试驾';
 			},
 			rightIcon() {
 				return this.routeType !== 'show' ? 'arrow' : ''
 			},
-			tryTimeStr() { // 试驾时间
-				return this.timeFormat({
-					type: 'datetime',
-					date: this.tryTimeHistory,
-					routeType: this.routeType,
-					value: '2019-09-23 14:15'
-				});
-			},
-			startTimeStr() { // 生效日期
-				return this.timeFormat({
-					type: 'date',
-					date: this.startTimeHistory,
-					routeType: this.routeType,
-					value: '2012-12'
-				});
-			},
-			endTimeStr() { // 截止日期
-				return this.timeFormat({
-					type: 'date',
-					date: this.endTimeHistory,
-					routeType: this.routeType,
-					value: '2022-12'
-				});
-			},
 			hasUploaded() {
 				return this.routeType === 'show' ? '已上传' : ''
 			},
+			actionItems() {
+				return this.select.items[this.select.currentSelectType];
+			},
 			timeSelectType() {
-				if (this.currentOperateTimeType === 'tryTime') {
+				if (this.date.currentOperateTimeType === 'tryTime') {
 					return 'datetime';
 				} else {
 					return 'date';
 				}
 			},
-			actionItems() {
-				if (this.currentSelectType === 'car') {
-					return [
-						{ name: '博越' },
-						{ name: '星越' },
-						{ name: '缤越' }
-					]
-				} else {
-					return [
-						{ name: '胡彦祖' },
-						{ name: '吴彦兵' },
-						{ name: '胡京' }
-					]
-				}
+			rule() {
+				return this.date.timeFormatRule[this.timeSelectType];
 			}
 		},
 		methods: {
 			goToUpload(type) {
 				this.$router.push(`/upload/${type}/operateType/${this.routeType}/detailId/${this.detailId}`);
 			},
-			showPopup(type) { // 显示时间选择框
-				this.currentOperateTimeType = type;
-				switch (type) {
-					case 'tryTime':
-						this.popupTimeSelect = this.tryTimeHistory;
-						break;
-					case 'startTime':
-						this.popupTimeSelect = this.startTimeHistory;
-						break;
-					case 'endTime':
-						this.popupTimeSelect = this.endTimeHistory;
-						break;
-					default:
-						break;
-				}
-				this.show = true;
-			},
 			toBeContinued() {
 				Dialog.alert({
 					message: '→ To Be Continued...'
 				})
 			},
+			/* 时间选择相关-start */
+			showPopup(type) { // 显示时间选择框
+				this.date.currentOperateTimeType = type;
+				this.date.popupTimeSelect = this.timeFormat(this.page[type], this.rule);
+				this.date.show = true;
+			},
 			dateConfirm(value) { // 确认时间选择
-				switch (this.currentOperateTimeType) {
-					case 'tryTime':
-						this.tryTimeHistory = value;
-						break;
-					case 'startTime':
-						this.startTimeHistory = value;
-						break;
-					case 'endTime':
-						this.endTimeHistory = value;
-						break;
-					default:
-						break;
-				}
-				this.show = false;
+				this.page[this.date.currentOperateTimeType] = this.timeFormat(value, this.rule);
+				this.dateCancel()
 			},
 			dateCancel() { // 取消时间选择
-				this.show = false;
+				this.date.show = false;
 			},
-			timeFormat({type, date, routeType, value}) { // Date转str
-				if (routeType === 'show') { // 判断是编辑页还是详情页
-					return value;
+			timeFormat(value, rule) {
+				if (!value) { // 为空时日期选择弹出来没有默认时间，默认 是默认的最小时间2009年1月1日
+					return null;
 				} else {
-					if (date !== null) {
-						let str = '';
-						let dateObj = {
-							year: date.getFullYear(),
-							month: date.getMonth() + 1,
-							day: date.getDate(),
-							hour: date.getHours(),
-							minute: date.getMinutes()
-						};
-						const { year, month, day, hour, minute } = dateObj;
-						const monthFormat = this.numberFormat(month);
-						const dayFormat = this.numberFormat(day);
-						const hourFormat = this.numberFormat(hour);
-						const minuteFormat = this.numberFormat(minute);
-						if (type === 'date') {
-							str = `${year}-${monthFormat}-${dayFormat}`;
-						} else {
-							str = `${year}-${monthFormat}-${dayFormat} ${hourFormat}:${minuteFormat}`
-						}
-						return str
-					} else {
-						return '';
+					if (value instanceof Date) { // 如果传进来的是日期对象 根据rule（类似YYYY-MM-DD HH:mm:ss）将其转成
+						return moment(value).format(rule);
+					} else if (typeof value === 'string') { // 如果是字符串 则转成日期对象
+						return new Date(moment(value, rule).valueOf());
 					}
 				}
 			},
-			numberFormat(number) {
-				return number < 10 ? `0${number}` : number;
+			getCurrentDateStr(rule) {
+				return moment(new Date()).format(rule);
 			},
+			setDefaultTime() {
+				this.page.tryTime = this.getCurrentDateStr(this.date.timeFormatRule.datetime); // 试驾时间默认为当天
+			},
+			/* 时间选择相关-end */
+
+			/* 类型选择-start */
 			openSelect(type) {
-				this.currentSelectType = type
-				this.selectShow = true;
+				this.select.currentSelectType = type
+				this.select.selectShow = true;
 			},
 			selectItem(item) { // 选择车型
-				this.selectShow = false;
-				this[this.currentSelectType] = item.name;
+				this.select.selectShow = false;
+				this.page[this.select.currentSelectType] = item.name;
 			},
+			/* 类型选择-end */
 			doTryCar() { // 立即试驾
 				this.toBeContinued();
 			}
