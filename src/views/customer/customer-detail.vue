c732c072512400c5f3a02a539caa1c7cb6743ed9
xiaosong.jiang <xiaosong.jiang@geely.com>
2019-09-26 16:38:38 +0800

    wip: 客户信息

--- src/views/customer/customer-detail.vue
+++ src/views/customer/customer-detail.vue
@@ -6,12 +6,12 @@
 					span 客户甲
 					span A
 					span 男
-				.base--card__title
+				.base--card__sub
 					.mobile 1361361336（浙江）
 					.create-time 创建:2019/08/22  |  来源：网络
 		van-cell-group
 			van-cell(title="意向车系" value="博瑞")
-			van-cell(title="意向车型" value="博瑞1.8T+6AT（国五）豪华型（博瑞）")
+			van-cell(title="意向车型" value="博瑞1.8T+6AT（国五）")
 			van-cell(title="车辆颜色" value="红色")
 			van-cell(title="网络定金" value="是")
 			van-cell(title="网络定单" value="是")
@@ -27,7 +27,7 @@
 			:style="{ height: '40%' }"
 			)
 			van-area(:area-list="_areaList"  title="所在地区")
-		van-button(type="primary" size="large") 编辑
+		van-button(color="#1B40D6" type="primary" size="large") 编辑
 </template>
 
 <script>
@@ -61,6 +61,27 @@
 </script>
 <style lang="scss" scoped>
 	.customer-detail {
+		&__warp {
+			padding: 15px;
+			.base--card {
+				color: #FFFFFF;
+				background-image: linear-gradient(135deg, #1B40D6 0%, #2273EF 100%);
+				border-radius: 6px;
+				box-shadow: 0 3px 4px 0 rgba(0, 110, 255, 0.2);
+				&__title {
+					padding: 0 15px;
+					font-size: 18px;
+					line-height: 45px;
+					height: 45px;
+					border-bottom: 1px solid rgba(255,255,255, 0.15);
+				}
+				&__sub {
+					padding: 10px 15px;
+					font-size: 14px;
+					line-height: 22px;
+				}
+			}
+		}
 
 	}
 </style>
