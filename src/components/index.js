c540e07297a81a6178b940502ce002eb580517b3
xiaosong.jiang <xiaosong.jiang@geely.com>
2019-09-29 15:05:03 +0800

    add: m-icon

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
-
+import MIcon from './m-icon'
 import MPage from './m-page'
 import IndexBarList from './index-bar-list'
+
+
 import MTimeSelect from './m-time-select'
 Vue.component(MPage.name, MPage)
 Vue.component(IndexBarList.name, IndexBarList)
 Vue.component(MTimeSelect.name, MTimeSelect)
+Vue.component(MIcon.name, MIcon)
---
+++ src/components/m-icon.vue
@@ -0,0 +1,24 @@
+<template lang='pug'>
+	svg.m-icon(aria-hidden="true")
+		use(:xlink:href="`#${iconClass}`")
+</template>
+
+<script>
+	export default {
+		name: 'm-icon',
+		props: {
+			iconClass: {
+				type: String,
+				default: ''
+			}
+		}
+	}
+</script>
+<style lang="scss" scoped>
+	.g-icon {
+		width: 1em; height: 1em;
+		vertical-align: -0.15em;
+		fill: currentColor;
+		overflow: hidden;
+	}
+</style>
