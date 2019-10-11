5040b0a738e198f95339724100aea72c2cc9f622
zhibin.lao <893435967@qq.com>
2019-10-11 08:51:40 +0800

    商机跟进列表页对接完成

--- src/views/niche-follow.vue
+++ src/views/niche-follow.vue
@@ -1,39 +1,39 @@
 <template lang="pug">
 	m-page.niche-follow
 		.head(:class="{ 'isOvertime': active === 3}")
-			van-tabs(v-model="active" title-active-color="#1B40D6" title-inactive-color="#333")
-				van-tab(title="今日(332)")
-				van-tab(title="明日(34)")
-				van-tab(title="更多(14)")
-				van-tab(title="超时(12)" class="red")
+			van-tabs(v-model="active" title-active-color="#1B40D6" title-inactive-color="#333" @change="changeTab")
+				van-tab(:title="`今日(${homePageData.todayCount || '-'})`")
+				van-tab(:title="`明日(${homePageData.tomorrowCount || '-'})`")
+				van-tab(:title="`更多(${homePageData.moreCount || '-'})`")
+				van-tab(:title="`超时(${homePageData.timeOutCount || '-'})`" class="red")
 		.container
 			div(style="height: 100%")
 				van-pull-refresh(v-model="isLoading" @refresh="onRefresh")
 					van-list(
 						v-model="loading"
 						:finished="finished"
-						finished-text="我是有底线的"
+						:finished-text="list.length === 0 && !loading ? '-暂无数据' : ''"
 						:immediate-check="false"
-						@load="onLoad"
+						@load="loadData"
 					)
 						.cell(
 							v-for="(item, index) in list"
 							:key="index"
 						)
 							.customer
-								span 张学友
-								span A
-								span 1小时前
-							.car 意向车型：博瑞1.8T+6AT(国五)豪华型（博瑞）
-							.latest-follow 最新跟进：2019/08/29 电话沟通
+								span {{item.pCustomerName}}
+								span {{dicMap[item.pCustomerLevel].dictValue.split('（')[0]}}
+								span {{item.remainingFollowUpMinutes}}分钟前
+							.car 意向车型：{{`${item.purposeSeriesName ? `${item.purposeSeriesName} ` : ''}${item.purposeModelName || '暂无信息'}`}}
+							.latest-follow 最新跟进：{{item.latestRecord}}
 							.operation
-								.operation-button(@click="goToCustomer") 记录
+								.operation-button(@click="() => { goToCustomer(item.id) }") 记录
 								.operation-button 短信
 								.operation-button 电话
 </template>
 
 <script>
-	import { Dialog } from 'vant'
+	import moment from 'moment'
     export default {
         name: 'niche-follow',
 		props: {
@@ -42,48 +42,107 @@
 			}
 		},
 		mounted() {
-        	this.active = parseInt(this.type) - 1;
-			this.triggerLoad();
+        	this.active = parseInt(this.type) - 1
+			this.changeData()
+		},
+		computed: {
+        	api() {
+				return this.$api.clueCustomer
+			},
+			homePageData() {
+				return this.$store.state.homePageData
+			},
+			dicMap() {
+        		return this.$store.state.dicMap
+			}
 		},
 		data() {
 			return {
 				active: 0,
 				list: [],
+				param: {},
+				defaultParam: {
+					pageNum: 1,
+					pageSize: 20
+				},
+				formatter: 'YYYY-MM-DD',
+				startTime: ' 00:00:00',
+				endTime: ' 23:59:59',
 				loading: false,
 				isLoading: false,
-				finished: false
+				finished: true // 要分页时改成true
 			}
 		},
 		methods: {
-			onLoad() {
-				// 异步更新数据
-				setTimeout(() => {
-					for (let i = 0; i < 5; i++) {
-						this.list.push(this.list.length + 1);
-					}
-					// 加载状态结束
-					this.loading = false;
+			changeTab() {
+				this.changeData()
+			},
+			changeData() {
+				let newParam = {
+					// ...this.defaultParam
+				}
+				let dateStr = ''
+				switch (this.active) {
+					case 0: // 今日
+						dateStr = moment().format(this.formatter)
+						newParam.startTime = `${dateStr}${this.startTime}`
+						newParam.endTime = `${dateStr}${this.endTime}`
+						break
+					case 1: // 明日
+						dateStr = moment().add(1, 'days').format(this.formatter)
+						newParam.startTime = `${dateStr}${this.startTime}`
+						newParam.endTime = `${dateStr}${this.endTime}`
+						break
+					case 2: // 更多
+						dateStr = moment().add(2, 'days').format(this.formatter)
+						newParam.startTime = `${dateStr}${this.startTime}`
+						newParam.endTime = ''
+						break
+					case 3: // 超时
+						newParam.timeoutFlag = '2'
+						break
+					default:
+						break
+				}
+				this.param = newParam
+				this.loading = true
+				/* this.finished = false */ // 要分页时取消该注释
+				this.list = []
 
-					// 数据全部加载完成
-					if (this.list.length >= 15) {
+				this.loadData()
+			},
+			loadData() {
+				this.api.query(this.param).then((data) => {
+					if (data.length > 0) {
+						// console.log('data', data)
+						for (let item of data) {
+							this.list.push(item)
+						}
+						// this.param.pageNum++ // 要分页时取消该注释
+					} else {
 						this.finished = true;
 					}
-				}, 500);
+				}).catch((error) => {
+					this.finished = true
+					this.$dialog.alert({
+						message: error.message
+					})
+				}).finally(() => {
+					this.loading = false;
+				})
 			},
 			triggerLoad() {
-				this.loading = true;
-				this.onLoad()
+				this.loading = true
+				this.loadData()
 			},
-			goToCustomer() {
-				Dialog.alert({
-					message: '→ To be continued'
-				})
+			goToCustomer(id) {
+				this.$router.push(`/customer/${id}/edit`)
 			},
 			onRefresh() {
 				setTimeout(() => {
-					this.$toast('刷新成功');
+					this.$toast('刷新成功')
 					this.isLoading = false;
-				}, 500);
+				}, 500)
 			}
 		}
     }
@@ -99,6 +158,12 @@
 		bottom: 0;
 		background-color: $common-grey;
 		overflow: scroll;
+		.empty-list-tip {
+			color: #969799;
+			font-size: 14px;
+			line-height: 50px;
+			text-align: center;
+		}
 		.cell {
 			margin-top: 10px;
 			padding: 15px;
